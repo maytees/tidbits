21946865a6b908c70a41c4654d3aba740580b524
maytees <mateespublicprofile@gmail.com>
2024-10-19 15:25:27 -0400

    init with nativewind & react native shadcn

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { MoonStar } from "~/lib/icons/MoonStar";
+import { Sun } from "~/lib/icons/Sun";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Edit tes skibidiapp/index.tsx to edit this screen.</Text>
+      <MoonStar />
+      <Sun />
+    </View>
+  );
+}
